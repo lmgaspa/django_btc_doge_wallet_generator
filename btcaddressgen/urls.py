7f392ddf28ee9bd7631d08fbd2106c107ab09f85
Luiz <seu-email@example.com>
2024-06-22 14:04:15 -0300

    past

--- btcaddressgen/urls.py
+++ btcaddressgen/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),  # Página inicial,
     path('', include('btcaddressgenerator.urls')),
+    path('btcaddressgenerator/', include('btcaddressgenerator.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 """
