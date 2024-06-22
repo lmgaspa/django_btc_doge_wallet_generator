8edc7f197b2fa26b2c60de3eb53a57c29c7986d6
Luiz <seu-email@example.com>
2024-06-22 15:43:19 -0300

    entendi

--- btcaddressgenerator/urls.py
+++ btcaddressgenerator/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('btcaddressgenerator/', views.btcaddressgenerator, name='btcaddressgenerator'),
+    path('', views.btcaddressgenerator, name='btcaddressgenerator'),
 ]
