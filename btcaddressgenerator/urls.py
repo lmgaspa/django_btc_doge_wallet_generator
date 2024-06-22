2c462600d5a9edb7b7bfbe07306c48ace1e991b5
Luiz <seu-email@example.com>
2024-06-22 15:10:17 -0300

    genbtcnoid

--- btcaddressgenerator/urls.py
+++ btcaddressgenerator/urls.py
@@ -3,11 +3,5 @@ from . import views
 from . import viewsnoid  # Importa viewsnoid do arquivo viewsnoid.py
 
 urlpatterns = [
-    
     path('viewsnoid/', viewsnoid.viewsnoid, name='viewsnoid'),  # Usa viewsnoid.viewsnoid para acessar a função de view
 ]
-
-
-"""
- path('', viewsnoid.viewsnoid, name='index'),
- """
